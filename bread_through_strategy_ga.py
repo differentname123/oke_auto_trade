f724c8582243eed69542b9eeab3b5b4975b588fe
differentname123 <2434954452@qq.com>
2025-05-04 10:40:26 +0800

    2025-05-02:     已完成:         1.完成统一的run实盘操作     待完成：         1.跑完所有的币种，进行最终的运行

--- bread_through_strategy_ga.py
+++ bread_through_strategy_ga.py
@@ -1451,7 +1451,7 @@ def example():
     data_path_list = [
         # "kline_data/origin_data_1m_10000000_BTC-USDT-SWAP.csv",
         # "kline_data/origin_data_1m_10000000_SOL-USDT-SWAP.csv",
-        "kline_data/origin_data_1m_10000000_ETH-USDT-SWAP.csv",
+        # "kline_data/origin_data_1m_10000000_ETH-USDT-SWAP.csv",
         "kline_data/origin_data_1m_10000000_TON-USDT-SWAP.csv",
         "kline_data/origin_data_1m_10000000_DOGE-USDT-SWAP.csv",
         "kline_data/origin_data_1m_10000000_XRP-USDT-SWAP.csv",
